55223e613fa692ed16c499c2f2f703ffc5663fa4
sho-itsuka <sho.itsuka122@gmail.com>
2022-05-22 20:07:29 +0900

    カード登録２

---
+++ app/javascript/card.js
@@ -0,0 +1,18 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); //環境変数を読み込む
+  const submit = document.getElementById("button"); //変数submitにid"button"の要素を取得し代入
+  submit.addEventListener("click", (e) => { //イベント発火
+    e.preventDefault(); //Railsのフォーム送信処理をキャンセルし、JavaScriptからサーバーサイドに値を送るようにする記述
+
+    //カード情報の取得先
+    const formResult = document.getElementById("charge-form"); //カード登録の入力フォームに付与されているid名
+    const formData = new FormData(formResult);
+
+    const card = {  //カードオブジェクトを生成
+      number: formData.get("number"),               //カード番号
+      cvc: formData.get("cvc"),                     //カード裏面の3桁の番号
+      exp_month: formData.get("exp_month"),         //有効期限の月
+      exp_year: `20${formData.get("exp_year")}`,    //有効期限の年
+    };
+  });
+};
